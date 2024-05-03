0e2d6f94cd634597ac0686f8d1cb36b8eddb7fbe
shimmail <shimmail@126.com>
2024-05-03 20:04:44 +0800

    sm commit

--- src/main/java/com/equestria/criticalskills/criticalskills/config/RedisConfig.java
+++ src/main/java/com/equestria/criticalskills/criticalskills/config/RedisConfig.java
@@ -26,3 +26,22 @@ public class RedisConfig {
 
 
 */
+package com.equestria.criticalskills.criticalskills.config;
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@MapperScan("com.equestria.criticalskills.criticalskills.mapper")
+public class RedisConfig {
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor(){
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
--- src/main/java/com/equestria/criticalskills/criticalskills/service/userService/userServiceImpl/UserServiceImpl.java
+++ src/main/java/com/equestria/criticalskills/criticalskills/service/userService/userServiceImpl/UserServiceImpl.java
@@ -1,20 +1,25 @@
 package com.equestria.criticalskills.criticalskills.service.userService.userServiceImpl;
 
 import cn.hutool.core.bean.BeanUtil;
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.equestria.criticalskills.criticalskills.exception.AccountException;
 import com.equestria.criticalskills.criticalskills.mapper.userMapper.AccountMapper;
 import com.equestria.criticalskills.criticalskills.mapper.userMapper.UserBasicInfoMapper;
+import com.equestria.criticalskills.criticalskills.mapper.userMapper.UserMapper;
 import com.equestria.criticalskills.criticalskills.pojo.commonPojo.DTO.LoginDTO;
 import com.equestria.criticalskills.criticalskills.pojo.commonPojo.DTO.RegisterDTO;
 import com.equestria.criticalskills.criticalskills.pojo.userPojo.userDTO.ForgetByEmailDTO;
 import com.equestria.criticalskills.criticalskills.pojo.userPojo.userDTO.ForgetBySecurityDTO;
 import com.equestria.criticalskills.criticalskills.pojo.userPojo.userEntity.Account;
+import com.equestria.criticalskills.criticalskills.pojo.userPojo.userEntity.User;
 import com.equestria.criticalskills.criticalskills.pojo.userPojo.userEntity.UserBasicInfo;
 import com.equestria.criticalskills.criticalskills.service.userService.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -22,12 +27,13 @@ import java.util.regex.Pattern;
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class UserServiceImpl implements UserService {
+public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements UserService {
 
     private final AccountMapper accountMapper;
     private final UserBasicInfoMapper userBasicInfoMapper;
 
-
+    @Autowired
+    private UserMapper userMapper;
 
      /*
      * TODO 邮箱验证码验证(基础验证,核对申请邮箱,验证码过期时间)
@@ -119,6 +125,59 @@ public class UserServiceImpl implements UserService {
         }
 
     }
+
+    //修改用户
+    @Override
+    @Transactional
+    public int updateUser(User user) {
+        return baseMapper.updateById(user);
+    }
+
+    //清空用户
+    @Override
+    public void clearUser(Long id) {
+        UpdateWrapper<User> updateWrapper = new UpdateWrapper<>();
+        // 假设除了id和username外，User实体类还有email和phone字段
+        updateWrapper.set("name",null)
+                .set("gender",null)
+                .set("image",null)
+                .set("video",null)
+                .set("birthday",null)
+                .set("email", null)
+                .set("phone", null)
+                .set("province",null)
+                .set("city",null)
+                .set("introduce",null)
+                .eq("id", id);
+
+        // 执行更新操作，只更新除了id和username之外的字段
+        userMapper.update(null, updateWrapper);
+    }
+
+    //上传图片
+    @Override
+    public void uploadImage(Long id, String url) {
+        UpdateWrapper<User> updateWrapper = new UpdateWrapper<>();
+        updateWrapper.set("image", url)
+                .eq("id", id);
+        userMapper.update(null, updateWrapper);
+    }
+
+    //上传视频
+    @Override
+    public void uploadVideo(Long id, String url) {
+        UpdateWrapper<User> updateWrapper = new UpdateWrapper<>();
+        updateWrapper.set("video", url)
+                .eq("id", id);
+        userMapper.update(null, updateWrapper);
+    }
+
+    //根据id返回用户信息
+    @Override
+    public User getUserById(Long id) {
+        return baseMapper.selectById(id);
+    }
+
 
 
 }
